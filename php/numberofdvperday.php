c96d87e242baba14144ec2c4d9770e288a95274f
Carlo Carabeo <carlo.carabeo@gmail.com>
2014-05-07 15:47:01 +0800

    number of dv per day script

---
+++ php/numberofdvperday.php
@@ -0,0 +1,13 @@
+<?php
+	require 'php/connect.php';
+
+	$date = $_POST['date'];;
+	$q = "SELECT COUNT(  `dv_num` ) 
+		FROM `disbursement_tbl`
+		WHERE `date_receive` = '" . $date . "'";
+	
+	$fin = mysql_fetch_array(mysql_query($q));
+	
+	
+	echo $fin[0];
+?>
